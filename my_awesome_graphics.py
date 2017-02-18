0b814c61355ae19112024624c8dc46d0f6fdfc3e
Barclay Koin <barclaykoin@gmail.com>
2017-02-18 14:36:30 +0300

    functin based

---
+++ my_awesome_graphics.py
@@ -0,0 +1,32 @@
+import turtle
+
+
+def func(what_to_do):
+    for i in range(1, 5):
+        what_to_do.forward(100)
+        what_to_do.left(90)
+
+
+def graphic_illustrations():
+    # create a windows screen
+    window = turtle.Screen()
+    window.bgcolor("red")
+    # create a turtle Tod who draws a square
+    tod = turtle.Turtle()
+    tod.color('green')
+    tod.shape('turtle')
+    tod.speed(1)
+    func(tod)
+    # create a turtle Angela who draws a circle
+    angela = turtle.Turtle()
+    angela.color("blue")
+    angela.circle(100)
+    # create a turtle Brad who draws a Triangle
+    brad = turtle.Turtle()
+    brad.color("blue")
+    brad.shape("classic")
+
+    window.exitonclick()
+
+
+graphic_illustrations()
